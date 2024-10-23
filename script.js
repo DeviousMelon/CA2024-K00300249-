893842a2ea3e4dc70587b485dfed3a258e97bfde
K00300249: Lauren Wright <K00300249@student.tus.ie>
2024-10-23 15:14:23 +0100

    Added all CA files

---
+++ script.js
@@ -0,0 +1,15 @@
+//<button id="fetchDataButton">Fetch Random Image</button>
+//<div id="dataContainer" class="data-box">
+
+function displayImage() {
+    fetch ('https://picsum.photos/400/300?random=${Math.random()}');
+    addEventListener('click')
+
+
+    //getElementbyId??
+    //QuerySelector
+
+}
+
+
+
